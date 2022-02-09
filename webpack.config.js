f8de58afc9f6738295d53b8e6461e9796d6a83a5
Vladyslav Andrievych <vladandrievych@gmail.com>
2022-02-09 13:39:28 +0200

    FET lesson7 task2 add webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /.(png|jpg)$/,
@@ -28,4 +35,14 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
 };
