fdc6f32e73948835a16120ce4be5da40bee72933
Vladyslav Andrievych <vladandrievych@gmail.com>
2022-02-10 14:07:40 +0200

    FET lesson 8 task 2 add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,45 +4,69 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /.s?css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /.(png|jpg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-              name: '[name].[ext]',
-              outputPath: 'images',
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  const config = {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /.s?css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /.(png|jpg)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+                name: '[name].[ext]',
+                outputPath: 'images',
+              },
             },
-          },
-        ],
-      },
-      {
-        test: /.js$/,
-        use: ['babel-loader'],
-      },
+          ],
+        },
+        {
+          test: /.js$/,
+          use: ['babel-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-  ],
+    devServer: {
+      port: 9000,
+      hot: true,
+      open: {
+        app: {
+          name: 'chrome',
+        },
+      },
+    },
+  };
+
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+      new CleanWebpackPlugin()
+    );
+  }
+
+  return config;
 };
